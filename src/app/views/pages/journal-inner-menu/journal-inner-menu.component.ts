d586a05cb47023484946238732dcb2b52b37b567
Kumar Jatinder FSD <developer.jatinderk@gmail.com>
2025-03-05 11:35:29 +0530

    fixed innerMenu Logincheck

--- src/app/views/pages/journal-inner-menu/journal-inner-menu.component.ts
+++ src/app/views/pages/journal-inner-menu/journal-inner-menu.component.ts
@@ -37,41 +37,16 @@ export class JournalInnerMenuComponent implements OnInit {
   ngOnInit(): void {
     var BookId = this.route.snapshot.params['Id'];
     var name = this.route.snapshot.params['name'];
-    this.LoginStatus = this.checkUserLogin();
-    if (!this.LoginStatus) {
-      this.cookieService.delete('authData');
-      this.cookieService.delete('BookData');
-      this.cookieService.deleteAll();
-
-      sessionStorage.clear();
-      localStorage.clear();
-
-      this.AuthSession.clearSession();
-      this.StoragesServices.clean();
-      this.VisitUrl(BookId, name, 'ExternalLogin');
-      return;
-    }
-    else {
+    this.LoginStatus=this.checkUserLogin();
+    if (BookId != undefined && this.LoginStatus === true ) {
       this.BookId = BookId;
       this.name = name;
     }
-
-
-    // var BookId = this.route.snapshot.params['Id'];
-    // var name = this.route.snapshot.params['name'];
-    // this.LoginStatus=this.checkUserLogin();
-    // var status=this.StoragesServices.isLoggedIn();
-    // if (BookId != undefined && this.LoginStatus === true && status==true) {
-    //   this.BookId = BookId;
-    //   this.name = name;
-    // }
-    // else if(status==false)
-    // {
-    //   this.BookId = BookId;
-    //   this.name = name;
-    //   this.LoginStatus=false;
-    //   // this.Logout();
-    // }
+    else if(this.LoginStatus===false)
+    {
+      this.BookId = BookId;
+      this.name = name;
+    }
   }
   checkUserLogin() {
     const GetCookieData = this.cookieService.get('authData');
